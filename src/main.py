f9b9dca5190948fbabfddcfa7bd068a5119d4e4e
Dylan <d.barva5402@gmail.com>
2020-09-14 19:55:30 -0700

    add my classes to notion

--- src/main.py
+++ src/main.py
@@ -3,9 +3,17 @@ from course import Course
 import quarter
 import tokens
 
-classes = [Course("CSE 11", datetime.time(hour=14, minute=0), datetime.time(hour=16, minute=0), ["M", "W", "F"]),
-           Course("Math 20C", datetime.time(hour=17), datetime.time(hour=18), ["M", "W", "F"])]
-q = quarter.Quarter(datetime.date(month=9, day=12, year= 2020), datetime.date(month=10, day=19, year= 2020), classes)
+classes = [Course("CSE 20 Lecture", datetime.time(hour=8, minute=0), datetime.time(hour=8, minute=50), ["M", "W", "F"]),
+           Course("CSE 20 Discussion", datetime.time(hour=13, minute=0), datetime.time(hour=13, minute=50), ["M"]),
+           Course("Math 20C Lecture", datetime.time(hour=9, minute=0), datetime.time(hour=9, minute=50), ["M", "W", "F"]),
+           Course("Math 20C Discussion", datetime.time(hour=17), datetime.time(hour=17, minute=50), ["M"]),
+           Course("CSE 11 Lecture", datetime.time(hour=10, minute=0), datetime.time(hour=10, minute=50), ["M", "W", "F"]),
+           Course("CSE 11 Discussion", datetime.time(hour=8, minute=0), datetime.time(hour=8, minute=50), ["T"]),
+           Course("WCWP 10A", datetime.time(hour=17, minute=0), datetime.time(hour=18, minute=20), ["T", "Th"]),
+
+           ]
+
+q = quarter.Quarter(datetime.date(month=10, day=1, year= 2020), datetime.date(month=12, day=11, year= 2020), classes)
 # q.get_all_meetings()
 # print(classes[0].get_notion_dates())
 q.add_to_notion(tokens.token_v2, tokens.url)
